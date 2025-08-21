9c55d8ff2c089a6285da19199750e66e69de965f
Matheus <matheus_lopes151@hotmail.com>
2025-08-21 20:51:55 -0300

    zod and react-hook-form

--- src/app/dashboard/me/_components/url.tsx
+++ src/app/dashboard/me/_components/url.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
 export function UrlPreview() {
+
+
+    async function submitAction(formData: FormData) {
+        const username = formData.get("username") as string
+
+        if (username === "") {
+            return
+        }
+
+
+    }
+
     return (
         <div className="flex items-center flex-1 p-2 text-gray-100">
-            <form>
+            <form className="flex flex-1 flex-col md:flex-row gap-4 items-start md:items-center"
+                action={submitAction}
+            >
                 <div className="flex items-center justify-center w-full">
                     <p
                         className="w-fit h-9 rounded-md flex items-center font-semibold text-white"
@@ -10,9 +27,18 @@ export function UrlPreview() {
                     </p>
                     <input
                         type="text"
-                        className="flex-1 outline-none border h-9 border-gray-300 text-black rounded-md"
+                        className="flex-1 outline-none border h-9 border-gray-300 bg-gray-50 text-black rounded-md px-1"
+                        placeholder="Digite seu username..."
+                        name="username"
                     />
                 </div>
+
+                <Button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md cursor-pointer"
+                >
+                    Salvar
+                </Button>
             </form>
         </div>
     )
